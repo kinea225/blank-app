87cac693e2d5e71fd3fe018900b6b833cf8c4c6a
kinea225 <115758314+kinea225@users.noreply.github.com>
2025-05-26 01:14:54 +0000

    데이터 표현하기

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+'''
+#문서작성하기
 import streamlit as st
 
 st.title("🎈 안녕하세요")
@@ -27,4 +29,24 @@ st.dataframe(df)
 st.header("3. 이미지 출력")
 
 st.image("https://i.namu.wiki/i/4El7Omx8MUNbvgPh06rSi50cTR5HI9QF3x8KuRAibfxEj6z-3Yqo19bi7pFUwyo73MaFIyibjmyibkq3Z8yzuXfFpPZ4siVz_OjZhEsyDmlSc6sb4Bq5OFsqW28zfqBWKgg5pVqwTIt4tcB6vjVR_Q.webp", width=300)
+'''
+
+#데이터 표현하기
+import streamlit as st
+st.set_page_config(page_title="문서 작성 예제", layout="centered")
+
+# 제목
+st.title("📘 Streamlit 문서 작성 예제")
+
+# 섹션
+st.header("1. 기본 텍스트 출력")
+
+# 표 출력
+st.header("2. 표 출력")
+import pandas as pd
+df = pd.DataFrame({
+    "이름": ["홍길동", "이몽룡"],
+    "나이": [29, 34]
+})
+st.dataframe(df)
 
